d75499eeff51fbd17abcf1dd69ae503f2ce1a07b
SulavChhetri <sulavthapachhetri@gmail.com>
2022-11-08 16:09:38 +0545

    final

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ assert r.status_code == 200
 
 cookie_url ='https://httpbin.org/cookies'
 rc =requests.get(cookie_url)
+assert rc.status_code == 200
 assert json.loads(rc.text)=={
     "cookies": {}
 }
 
 cookie_del_url = 'https://httpbin.org/cookies/delete?freeform=anotherone'
 rcd = requests.get(cookie_del_url)
+assert rcd.status_code == 200
 assert json.loads(rcd.text) == {
     "cookies": {}
 }
 
 cookie_set = 'https://httpbin.org/cookies/set?freeform=sulav'
 rcs = requests.get(cookie_set)
+assert rcs.status_code == 200
 assert json.loads(rcs.text) == {
     "cookies": {
         "freeform": "sulav",
@@ -28,6 +31,7 @@ assert json.loads(rcs.text) == {
 
 cookie_name_set = 'https://httpbin.org/cookies/set/name/sulavthapa'
 rcns = requests.get(cookie_name_set)
+assert rcns.status_code == 200
 assert json.loads(rcns.text) == {
     "cookies": {
         "name": "sulavthapa"
